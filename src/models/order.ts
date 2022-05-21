8d3422e0f309eaed7649ddc0585201f6f560516d
coreminw <coreminw@naver.com>
2022-05-21 15:31:49 +0900

    init: set model 추가

---
+++ src/models/order.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+import { OrderInfo } from "../interfaces/order/OrderInfo";
+import user from "./user";
+
+const OrderSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    ref: user,
+  },
+  productName: {
+    type: String,
+  },
+  orderDate: {
+    type: Date,
+  },
+  payMethod: {
+    type: String,
+  },
+});
+
+export default mongoose.model<OrderInfo & mongoose.Document>("Order", OrderSchema);
