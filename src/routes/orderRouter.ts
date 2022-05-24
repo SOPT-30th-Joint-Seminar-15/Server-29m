d218425570c535b64f035be597aae5f33d4d15cb
soryeongk <soryeongk@yolocorp.kr>
2022-05-24 20:35:08 +0900

    bad code: delete files for renaming - 이 커밋으로 되돌아오지 말기

--- src/routes/orderRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import orderController from "../controllers/orderController";
-
-const router: Router = Router();
-
-router.get("/:order", orderController.getOrderInfo);
-
-export default router;
