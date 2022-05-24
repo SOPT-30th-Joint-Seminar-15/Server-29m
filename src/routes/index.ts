935d81580c6833d270d51b7bfe2dec7e6b044ca8
soryeongk <soryeongk@yolocorp.kr>
2022-05-24 20:19:19 +0900

    feat: router 연결

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,9 @@
 //router index file
 import { Router } from "express";
+import OrderRouter from './orderRouter';
 
 const router = Router();
+
+router.use("/order", OrderRouter);
 
 export default router;
