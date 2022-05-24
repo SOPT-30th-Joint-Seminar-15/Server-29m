507bb7176fb2612da5918cae84f159673ec0f186
soryeongk <soryeongk@yolocorp.kr>
2022-05-24 19:08:15 +0900

    feat: 빈 컬렉션 생성해두기

--- src/loaders/db.ts
+++ src/loaders/db.ts
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 
 import config from "../config";
+import inquiryInfo from "../models/inquiryInfo";
+import order from "../models/order";
+import user from "../models/user";
 
 const connectDB = async () => {
   try {
@@ -8,7 +11,19 @@ const connectDB = async () => {
 
     mongoose.set("autoCreate", true);
 
-    console.log("Mongoose Connected ...");
+    console.log("🌸 몽구몽구스 연결 완료! 🌸");
+
+    user.createCollection().then(function () {
+      console.log("📌 User Collection is created!");
+    });
+
+    order.createCollection().then(function () {
+      console.log("📌 Order Collection is created!");
+    });
+
+    inquiryInfo.createCollection().then(function () {
+      console.log("📌 InquiryInfo Collection is created!");
+    });
   } catch (err: any) {
     console.error(err.message);
     process.exit(1);
