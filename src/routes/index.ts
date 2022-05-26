b6cd6320294a22032c6b884248c97267f4b00ff7
soryeongk <soryeongk@yolocorp.kr>
2022-05-26 12:12:41 +0900

    feat: inquiryId로 문의 삭제 라우터 연결스!

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,11 @@
 import { Router } from "express";
 
 import OrderRouter from "./order";
+import InquiryRouter from "./inquiry";
 
 const router = Router();
 
 router.use("/order", OrderRouter);
+router.use("/inquiry", InquiryRouter);
 
 export default router;
---
+++ src/routes/inquiry.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { inquiryController } from "../controllers";
+
+const router: Router = Router();
+
+router.delete("/:inquiryId", inquiryController.deleteInquiry);
+
+export default router;
